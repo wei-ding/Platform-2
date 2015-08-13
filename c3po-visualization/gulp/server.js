ee9095698f1b959aab2f8142876919d1df682fef
Wei Ding <vvei.ding@gmail.com>
2015-08-13 00:10:58 -0400

    new file:   gulp/build.js 	new file:   gulp/e2e-test.js 	new file:   gulp/inject.js 	new file:   gulp/proxy.js 	new file:   gulp/server.js 	new file:   gulp/styles.js 	new file:   gulp/translate.js 	new file:   gulp/unit-tests.js 	new file:   gulp/watch.js

---
+++ c3po-visualization/gulp/server.js
@@ -0,0 +1,70 @@
+ /*jshint unused:false */
+
+/***************
+
+  This file allow to gulp run BrowserSync in order to serving and watching
+  files from the web project
+
+* @author Wei Ding
+* @version 0.1.0
+***************/
+/*!
+    This content is released under the (http://opensource.org/licenses/MIT) MIT License.
+
+    The MIT License (MIT)
+
+    Copyright (c) <2015> <>
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. * MIT 
+*/
+'use strict';
+
+var gulp = require('gulp');
+
+var paths = gulp.paths;
+
+var util = require('util');
+
+var browserSync = require('browser-sync');
+
+var middleware = require('./proxy');
+
+function browserSyncInit(baseDir, files, browser) {
+  browser = browser === undefined ? 'default' : browser;
+
+  var routes = null;
+  if(baseDir === paths.src || (util.isArray(baseDir) && baseDir.indexOf(paths.src) !== -1)) {
+    routes = {
+      '/bower_components': 'bower_components'
+    };
+  }
+
+  browserSync.instance = browserSync.init(files, {
+    startPath: '/',
+    server: {
+      baseDir: baseDir,
+      middleware: middleware,
+      routes: routes
+    },
+    browser: browser
+  });
+}
+
+gulp.task('serve', ['watch'], function () {
+  browserSyncInit([
+    paths.tmp + '/serve',
+    paths.src
+  ], [
+    paths.tmp + '/serve/{app,components}/**/*.css',
+    paths.src + '/{app,components}/**/*.js',
+    paths.src + '/assets/images/**/*',
+    paths.tmp + '/serve/*.html',
+    paths.tmp + '/serve/{app,components}/**/*.html',
+    paths.src + '/{app,components}/**/*.html'
+  ]);
+});
+
