a5a37fde13a9824cc54d334fca3706eb75590b2f
Wei Ding <vvei.ding@gmail.com>
2016-02-11 09:10:31 -0500

    modified:   dev/Clinical3PO-Vagrant/modules/knox_ldap/manifests/init.pp

--- dev/Clinical3PO-Vagrant/modules/knox_ldap/manifests/init.pp
+++ dev/Clinical3PO-Vagrant/modules/knox_ldap/manifests/init.pp
@@ -16,6 +16,8 @@
 include stdlib
 include ldap
 
+$testdata = "output-uids.ldif"
+
 # ldap model
 class knox_ldap {
 
@@ -27,5 +29,52 @@ class knox_ldap {
                 rootpw   => 'PWc3po',
                 auth_who => 'anonymous'
         }
+
+        ldap::define::schema {'brodate':
+        ensure => present,
+        source => 'puppet:///modules/ldap/schema/brodate.schema',
+        }
+
+        ldap::define::schema {'sudo':
+        ensure => present,
+        source => 'puppet:///modules/ldap/schema/sudo.schema',
+        }
+
+        ldap::define::schema {'misc':
+        ensure => present,
+        source => 'puppet:///modules/ldap/schema/misc.schema',
+        }
+
+        ldap::define::schema {'openssh':
+        ensure => present,
+        source => 'puppet:///modules/ldap/schema/openssh.schema',
+        }
+
+        ldap::define::schema {'inetorgperson':
+        ensure => present,
+        source => 'puppet:///modules/ldap/schema/inetorgperson.schema',
+        }
+
+        file {
+        '/etc/ldap/schema/nis.schema':
+            ensure => file,
+            source => 'puppet:///modules/ldap/schema/nis.schema';
+        }
+
+        ldap::define::schema {'dyngroup':
+        ensure => present,
+        source => 'puppet:///modules/ldap/schema/dyngroup.schema',
+        }
+
+        exec {"$testdata":
+        require   => [Ldap::Define::Schema["dyngroup", "inetorgperson", "brodate", "sudo", "misc", "openssh"], File["/etc/ldap/schema/nis.schema"]], 
+        command   => "/usr/bin/ldapadd -w test -D 'cn=dsadmin,dc=brodate,dc=net' -H ldap://localhost -f /vagrant_data/deploy/${testdata}"
+        }
+
+        exec {'input_ldap_test_data.ldif':
+        require   => [Ldap::Define::Schema["dyngroup", "inetorgperson", "brodate", "sudo", "misc", "openssh"], File["/etc/ldap/schema/nis.schema"], Exec["$testdata"]], 
+        command   => '/usr/bin/ldapadd -w test -D "cn=dsadmin,dc=brodate,dc=net" -H ldap://localhost -f /vagrant_data/deploy/ldap_test_data.ldif -c'
+        }
+
 }
 
