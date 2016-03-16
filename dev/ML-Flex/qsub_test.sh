e6522143f8874435e16e8b8b91e91dda45be8f22
Wei Ding <vvei.ding@gmail.com>
2016-03-16 14:30:21 -0400

    Add qsub_test.sh to master branch under the ML-Flex project

--- dev/ML-Flex/qsub_test.sh
+++ dev/ML-Flex/qsub_test.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
+#/*
+# *	 Copyright 2016 Clinical3PO/dingw
+# *
+# *   Licensed under the Apache License, Version 2.0 (the "License");
+# *   you may not use this file except in compliance with the License.
+# *   You may obtain a copy of the License at
+# *
+# *       http://www.apache.org/licenses/LICENSE-2.0
+# *
+# *   Unless required by applicable law or agreed to in writing, software
+# *   distributed under the License is distributed on an "AS IS" BASIS,
+# *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# *   See the License for the specific language governing permissions and
+# *   limitations under the License.
+# */
 #PBS -j oe
 # OPENMPI will use torque-native mechainsms to launch and kill processes
 # This will run the 2 MPI processes on the nodes that were allocated by Torque
